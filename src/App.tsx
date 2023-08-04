a257866bfb53152f2f5f56fbc627a152a39dbec0
Daniel Filipe <42974630+DanielKitanaxiFilipe@users.noreply.github.com>
2023-08-04 14:40:11 +0100

    update th

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home";
+import Login from "./pages/login";
 function App() {
-  return <>Ola</>
+  return <>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+    </Routes>
+  </Router>
+  </>
 }
 export default App;
--- src/firebase/index.tsx
+++ src/firebase/index.tsx
@@ -1,7 +1,3 @@
-
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
 const firebaseConfig = {
   apiKey: "AIzaSyAfCjHoO8cyd9qCS-8sIyI8Ss_6Czgo_uw",
   authDomain: "threads-f5a5f.firebaseapp.com",
@@ -11,6 +7,4 @@ const firebaseConfig = {
   appId: "1:249791645842:web:ce35084aac6a590f3d7e4b",
   measurementId: "G-7Z67HH5LTK"
 };
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export default firebaseConfig
